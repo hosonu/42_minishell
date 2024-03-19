e9b3c027bc04be208bedc5a1b90dc1591e345377
渡辺耕次 <watanabekoji@sekainihitotsudakenoMacBook-Air.local>
2024-03-20 01:17:26 +0900

    first commit

---
+++ minishell.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kojwatan <kojwatan@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 11:31:28 by kojwatan          #+#    #+#             */
+/*   Updated: 2024/03/20 00:38:38 by kojwatan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "./libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+
+# define METACHAR 0
+# define OWOUTFILE 10
+# define APNDOUTFILE 11
+# define INFILE 20
+# define HEREDOC 21
+# define COMMAND 30
+# define REINCOMMAND 31
+# define REOUTCOMMAND 32
+# define REINTOUTCOMMAND 33
+# define PIPECOMMAND 34
+
+typedef struct s_token
+{
+	char			*token;
+	int	type;
+	struct s_token *next;
+	struct s_token *prev;
+}	t_token;
+
+int		token_len(char *src);
+int	is_metachar(char c);
+char	*tokenizer(char *str);
+char	*make_right(char *str);
+int	decide_type(t_token *top);
+char	*strdup_right(char *str);
+t_token	*new_token(char *content);
+t_token	*lexer(char *str);
+void	token_addlast(t_token *top, t_token *new);
+
+#endif
