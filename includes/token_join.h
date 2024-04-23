9ec78e4bbb5d744ac1d29e4575ea5bc1c7548061
渡辺耕次 <watanabekoji@sekainihitotsudakenoMacBook-Air.local>
2024-04-24 02:00:37 +0900

    ifdef name was wrong

--- includes/token_join.h
+++ includes/token_join.h
@@ -6,12 +6,12 @@
 /*   By: kojwatan <kojwatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 22:42:54 by kojwatan          #+#    #+#             */
-/*   Updated: 2024/04/18 22:43:46 by kojwatan         ###   ########.fr       */
+/*   Updated: 2024/04/24 02:00:09 by kojwatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TOKEN_DEFINE_H
-# define TOKEN_DEFINE_H
+#ifndef TOKEN_JOIN_H
+# define TOKEN_JOIN_H
 
 t_token	*token_join(t_token *top);
 
