6045fafada05f6896e54c789fa2fcc83691f3295
渡辺耕次 <watanabekoji@sekainihitotsudakenoMacBook-Air.local>
2024-04-24 23:30:41 +0900

    add special character validate

--- srcs/builtins/42_unset.c
+++ srcs/builtins/42_unset.c
@@ -6,24 +6,48 @@
 /*   By: kojwatan <kojwatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 08:31:30 by kojwatan          #+#    #+#             */
-/*   Updated: 2024/04/23 08:32:11 by kojwatan         ###   ########.fr       */
+/*   Updated: 2024/04/24 23:28:14 by kojwatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static int	unset_util(char *av)
+{
+	int	i;
+
+	i = 0;
+	while (av[i] != '\0')
+	{
+		if (is_special_char(av[i]) == 1)
+		{
+			ft_putstr_fd("unset: not a valid identifier\n", STDERR_FILENO);
+			return (-1);
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	ft_unset(char *av[], t_env *env)
 {
 	int		i;
+	int		ret;
 	t_env	*node;
 
 	i = 0;
+	ret = 1;
 	while (av[i] != NULL)
 	{
-		node = getenv_node(env, av[i]);
-		if (node != NULL)
-			delone_env(env, node);
+		if (unset_util(av[i]) == -1 || varname_validate(av[i], "unset") == -1)
+			ret = 1;
+		else
+		{
+			node = getenv_node(env, av[i]);
+			if (node != NULL)
+				delone_env(env, node);
+		}
 		i++;
 	}
-	return (0);
+	return (ret);
 }
